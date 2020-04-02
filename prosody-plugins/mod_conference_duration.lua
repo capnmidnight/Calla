3be7e5afa3399399110359a638a807c0d25902a6
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-04-02 12:55:43 -0400

    dump the junk and just make a Jitsi API demo

--- prosody-plugins/mod_conference_duration.lua
+++
@@ -1,5 +0,0 @@
-local conference_duration_component
-    = module:get_option_string(
-        "conference_duration_component", "conferenceduration"..module.host);
-
-module:add_identity("component", "conference_duration", conference_duration_component);
