9ad5c7b5d2f47d3151b51c7d3c16c1cb5c373541
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-04-04 00:51:47 -0400

    formatting

--- jitsihax.js
+++ jitsihax.js
@@ -1,39 +1,34 @@
-(function(){
+(function () {
 
-function setVolume(user, volume){
-  var id = "#participant_" + user + " audio",
-    audio = document.querySelector(id);
-  if(audio){
-    audio.volume = volume;
-  }
-  else{
-    console.warn("Could not find audio element for user " + user);
-  }
-}
-
-var commands = {
-  _setVolume: function(evt){
-    setVolume(evt.user, evt.volume);
-  }
-}
-
-addEventListener("message", function(evt) {
-  if(evt.origin === "http://localhost"
-    || evt.origin === "https://localhost"
-    || evt.origin.startsWith("http://localhost:")
-    || evt.origin.startsWith("https://localhost:")
-    || evt.origin === "https://meet.primrosevr.com")
-    try{
-      var data = JSON.parse(evt.data);
-      if(data.hax === "jitsi") {
-        var commandID = "_" + data.command;
-        if(commands[commandID]){
-           commands[commandID](data);
+    var commands = {
+        setVolume: function (evt) {
+            var id = "#participant_" + evt.user + " audio",
+                audio = document.querySelector(id);
+            if (audio) {
+                audio.volume = evt.volume;
+            }
+            else {
+                console.warn("Could not find audio element for user " + user);
+            }
         }
-      }
-    }
-    catch(exp){
-      console.error(exp);
-    }
-});
+    };
+
+    addEventListener("message", function (evt) {
+        if (evt.origin === "http://localhost"
+            || evt.origin === "https://localhost"
+            || evt.origin.startsWith("http://localhost:")
+            || evt.origin.startsWith("https://localhost:")
+            || evt.origin === "https://meet.primrosevr.com") {
+            try {
+                var data = JSON.parse(evt.data);
+                if (data.hax === "jitsi"
+                    && commands[data.command]) {
+                    commands[data.command](data);
+                }
+            }
+            catch (exp) {
+                console.error(exp);
+            }
+        }
+    });
 })();
