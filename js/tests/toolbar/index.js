aedb02057a1324f73b42ff25b3e9a8219d5a2639
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-06-20 12:05:58 -0400

    show events

--- js/tests/toolbar/index.js
+++ js/tests/toolbar/index.js
@@ -2,5 +2,32 @@
 // be able to test the UI without having to connect to a
 // meeting.
 import { ToolBar } from "../../src/forms/toolbar.js";
+import { P } from "../../src/html/tags.js";
 const toolbar = new ToolBar();
 document.body.appendChild(toolbar.element);
+
+let muted = false;
+
+toolbar.addEventListener("toggleaudio", (evt) => {
+    muted = !muted;
+    toolbar.setAudioMuted(muted);
+});
+
+function echo(evt) {
+    const label = P(evt.type);
+    document.body.appendChild(label);
+    setTimeout(() => {
+        document.body.removeChild(label);
+    }, 3000);
+}
+
+document.body.append(P("asdf"), P());
+
+toolbar.addEventListener("toggleaudio", echo);
+toolbar.addEventListener("emote", echo);
+toolbar.addEventListener("selectemoji", echo);
+toolbar.addEventListener("zoomchanged", echo);
+toolbar.addEventListener("options", echo);
+toolbar.addEventListener("tweet", echo);
+toolbar.addEventListener("leave", echo);
+toolbar.addEventListener("toggleui", echo);
