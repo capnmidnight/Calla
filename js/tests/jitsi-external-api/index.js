7c08e07ca7e1005921bdf99a8fd477e76977fe63
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-06-23 15:08:23 -0400

    unit testing of jitsi meet interface

--- js/tests/jitsi-external-api/index.js
+++ js/tests/jitsi-external-api/index.js
@@ -1,111 +1,165 @@
-﻿
-import "../../src/protos.js";
+﻿import "../../src/protos.js";
 import { HtmlTestOutput as TestOutput, TestCase } from "../../etc/assert.js";
 import { JitsiClient } from "../../src/jitsihax-client-external-api.js";
-import { P, Div, Button } from "../../src/html/tags.js";
-import { style } from "../../src/html/attrs.js";
-import { onClick } from "../../src/html/evts.js";
 
-class JitsiClientTests extends TestCase {
-    async test_toggleAudio() {
-        const evt = client.once("audioMuteStatusChanged", null, 1000);
+const userNumber = document.location.hash.length > 0
+    ? parseFloat(document.location.hash.substring(1))
+    : 1;
+
+class TestBase extends TestCase {
+    async joinChannel() {
+        await client.joinAsync("TestRoom", "TestUser" + userNumber);
+        const evt = await client.once("videoConferenceJoined");
+        this.hasValue(evt);
+        this.isEqualTo(evt.id, client.localUser);
+    }
+
+    async waitForJoin() {
+        if (client.otherUsers.size === 0) {
+            await client.once("participantJoined", 5000);
+        }
+
+        this.isGreaterThan(client.otherUsers.size, 0);
+        for (let id of client.otherUsers.keys()) {
+            this.hasValue(id);
+        }
+    }
+
+    async initUsers() {
+        const respond = (evt) => {
+            console.log("======================== RESPONDING");
+            client.userInitResponse(evt.id, { userNumber });
+        };
+
+        client.addEventListener("userInitRequest", respond);
+
+        const requests = [];
+        for (let id of client.otherUsers.keys()) {
+            requests.push(client.userInitRequestAsync(id));
+        }
+        const responses = await Promise.all(requests);
+        this.hasValue(responses);
+        this.isGreaterThan(responses.length, 0);
+
+        for (let response of responses) {
+            this.hasValue(response.id);
+            this.isNotEqualTo(response.id, client.localUser);
+
+            this.hasValue(response.data);
+            this.hasValue(response.data.userNumber);
+            this.isNotEqualTo(response.data.userNumber, userNumber);
+        }
+        client.removeEventListener("userInitRequest", respond);
+    }
+}
+
+class JitsiClient1_Tests extends TestBase {
+    async test_00_joinChannel() {
+        await this.joinChannel();
+    }
+
+    async test_01_getAudioOutputDevices() {
+        const audioOutputDevices = await client.getAudioOutputDevices();
+        this.hasValue(audioOutputDevices);
+        this.isGreaterThan(audioOutputDevices.length, 0);
+    }
+
+    async test_02_getCurrentAudioOutputDevice() {
+        const curAudioOut = await client.getCurrentAudioOutputDevice();
+        this.hasValue(curAudioOut);
+    }
+
+    async test_03_getAudioInputDevices() {
+        const audioInputDevices = await client.getAudioInputDevices();
+        this.hasValue(audioInputDevices);
+        this.isGreaterThan(audioInputDevices.length, 0);
+    }
+
+    async test_04_getCurrentAudioInputDevice() {
+        const curAudioIn = await client.getCurrentAudioInputDevice();
+        this.hasValue(curAudioIn);
+    }
+
+    async test_05_getVideoInputDevices() {
+        const videoInputDevices = await client.getVideoInputDevices();
+        this.hasValue(videoInputDevices);
+        this.isGreaterThan(videoInputDevices.length, 0);
+    }
+
+    async test_06_getCurrentVideoInputDevice() {
+        const curVideoIn = await client.getCurrentVideoInputDevice();
+        this.isNull(curVideoIn);
+    }
+
+    async test_07_toggleAudio() {
+        const evt = client.once("audioMuteStatusChanged", 1000);
         client.toggleAudio();
         await evt;
         this.success("audio mute status changed");
     }
 
-    async test_isAudioMuted() {
+    async test_08_isAudioMuted() {
         let muted = await client.isAudioMutedAsync();
         this.isBoolean(muted);
     }
 
-    async test_setAudioMuted() {
-        const evt = client.once("audioMuteStatusChanged", null, 1000);
+    async test_09_setAudioMuted() {
+        const evt = client.once("audioMuteStatusChanged", 1000);
         await client.setAudioMutedAsync(false);
         await evt;
         this.success("audio mute status changed");
     }
 
-    async test_toggleVideo() {
-        const evt = client.once("videoMuteStatusChanged", null, 1000);
+    async test_10_toggleVideo() {
+        const evt = client.once("videoMuteStatusChanged", 1000);
         client.toggleVideo();
         await evt;
         this.success("video mute status changed");
     }
 
-    async test_isVideoMuted() {
+    async test_11_isVideoMuted() {
         let muted = await client.isVideoMutedAsync();
         this.isBoolean(muted);
     }
 
-    async test_setVideoMuted() {
-        const evt = client.once("videoMuteStatusChanged", null, 1000);
+    async test_12_setVideoMuted() {
+        const evt = client.once("videoMuteStatusChanged", 1000);
         await client.setVideoMutedAsync(false);
         await evt;
         this.success("video mute status changed");
     }
+
+    async test_13_participantJoined() {
+        const loc = new URL(document.location.href);
+        loc.hash = "#" + (userNumber + 1);
+        window.open(loc.href, "_blank", "screenX:10,screenY:10,width:400,height:600");
+        await this.waitForJoin();
+    }
+
+    async test_14_initUser() {
+        await this.initUsers();
+    }
 }
 
-let hash = document.location.hash.length > 0
-    ? parseFloat(document.location.hash.substring(1))
-    : 1,
-    myID = null;
+class JitsiClient2_Tests extends TestBase {
+    async test_00_joinChannel() {
+        await this.joinChannel();
+    }
+
+    async test_01_participantJoined() {
+        await this.waitForJoin();
+    }
+
+    async test_14_initUser() {
+        await this.initUsers();
+    }
+}
 
 const client = new JitsiClient(),
-    output = Div(
-        style({
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            color: "#0f0",
-            pointerEvents: "none"
-        })),
-    spawn = Button(
-        style({
-            position: "absolute",
-            top: 0,
-            left: "calc(50% - 3.5em)",
-            width: "7em",
-            padding: "0.5em"
-        }),
-        onClick(() => {
-            const loc = new URL(document.location.href);
-            ++hash;
-            loc.hash = "#" + hash;
-            window.open(loc.href, "_blank", "width:800,height:600,screenX:10,screenY:10");
-        }),
-        "Spawn"),
-    users = new Map(),
-    cons = new TestOutput(JitsiClientTests);
-
-async function setup(evt) {
-
-    myID = evt.id;
-
-    const audioOutputDevices = await client.getAudioOutputDevices(),
-        audioInputDevices = await client.getAudioInputDevices(),
-        videoInputDevices = await client.getVideoInputDevices(),
-        curAudioOut = await client.getCurrentAudioOutputDevice(),
-        curAudioIn = await client.getCurrentAudioInputDevice(),
-        curVideoIn = await client.getCurrentVideoInputDevice();
-
-    echoValue(audioOutputDevices);
-    echoValue(audioInputDevices);
-    echoValue(videoInputDevices);
-    echoValue(curAudioOut);
-    echoValue(curAudioIn);
-    echoValue(curVideoIn);
-}
-
-async function runTest(evt) {
-    const state = await client.userInitRequestAsync(evt.id);
-    console.log("=============== GOT USER STATE");
-    echoEvt(state);
-
-    await cons.run();
-}
+    cons = new TestOutput(
+        userNumber === 1
+            ? JitsiClient1_Tests
+            : JitsiClient2_Tests);
 
 (async function () {
     const response = await fetch("../../index.html"),
@@ -118,39 +172,26 @@ async function runTest(evt) {
     document.body.append(
         ...doc.body.childNodes,
         client.element,
-        output);
+        cons.element);
 
-    if (hash === 1) {
-        document.body.append(cons.element);
+    Object.assign(client.element.style, {
+        bottom: undefined,
+        height: "50%"
+    });
 
-        Object.assign(client.element.style, {
-            bottom: undefined,
-            height: "50%"
-        });
-
-        Object.assign(cons.element.style, {
-            position: "absolute",
-            top: "50%",
-            left: 0,
-            width: "100%",
-            height: "50%"
-        });
-
-        document.body.append(spawn);
-    }
+    Object.assign(cons.element.style, {
+        position: "absolute",
+        top: "50%",
+        left: 0,
+        width: "100%",
+        height: "50%"
+    });
 
     document.body.removeChild(document.body.querySelector("#login"));
 
     client.addEventListener("moveTo", echoEvt);
     client.addEventListener("emote", echoEvt);
     client.addEventListener("userInitRequest", echoEvt);
-    client.addEventListener("userInitRequest", (evt) => {
-        client.userInitResponse(evt.participantID, {
-            id: myID,
-            x: 0,
-            y: 0
-        });
-    });
     client.addEventListener("userInitResponse", echoEvt);
     client.addEventListener("audioMuteStatusChanged", echoEvt);
     client.addEventListener("videoMuteStatusChanged", echoEvt);
@@ -162,30 +203,8 @@ async function runTest(evt) {
     client.addEventListener("displayNameChange", echoEvt);
     client.addEventListener("audioActivity", echoEvt);
 
-    client.addEventListener("participantJoined", (evt) => {
-        users.set(evt.id, evt.displayName);
-    });
-
-    client.addEventListener("participantLeft", (evt) => {
-        users.delete(evt.id);
-    });
-
-    if (hash === 1) {
-        client.addEventListener("videoConferenceJoined", setup);
-        client.addEventListener("participantJoined", runTest, { once: true });
-    }
-
-    await client.joinAsync("TestRoom", "TestUser" + hash);
+    cons.run();
 })();
-
-function echoValue(value) {
-    console.log("============== VALUE:", value);
-    const label = P(value);
-    output.appendChild(label);
-    setTimeout(() => {
-        output.removeChild(label);
-    }, 5000);
-}
 
 function echoEvt(evt) {
     console.log(`============ ECHO EVT ${evt.type} ==============`);
