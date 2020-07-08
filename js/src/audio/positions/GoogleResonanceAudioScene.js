56d2d2766a6715d1a82bf166a904a9f7ec69f389
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-07-07 23:42:10 -0400

    cleanup

--- js/src/audio/positions/GoogleResonanceAudioScene.js
+++ js/src/audio/positions/GoogleResonanceAudioScene.js
@@ -8,22 +8,24 @@ export class GoogleResonanceAudioScene extends InterpolatedPosition {
     constructor(audioContext) {
         super();
 
-        this.scene = new ResonanceAudio(audioContext);
+        this.scene = new ResonanceAudio(audioContext, {
+            ambisonicOrder: 3
+        });
         this.scene.output.connect(audioContext.destination);
 
         this.position = new InterpolatedPosition();
 
         this.scene.setRoomProperties({
-            width: 3.1,
-            height: 2.5,
-            depth: 3.4,
+            width: 10,
+            height: 5,
+            depth: 10,
         }, {
-            left: 'brick-bare',
-            right: 'curtain-heavy',
-            front: 'marble',
-            back: 'glass-thin',
-            down: 'grass',
-            up: 'transparent',
+            left: "transparent",
+            right: "transparent",
+            front: "transparent",
+            back: "transparent",
+            down: "grass",
+            up: "transparent",
         });
     }
 
--- js/src/math.js
+++ js/src/math.js
@@ -10,6 +10,13 @@ export function clamp(v, min, max) {
     return Math.min(max, Math.max(min, v));
 }
 
+/**
+ * 
+ * @param {number} a
+ * @param {number} b
+ * @param {number} p
+ * @returns {number}
+ */
 export function lerp(a, b, p) {
     return (1 - p) * a + p * b;
 }
