a7de148e255abb429cbc4208752c3d51dd11c724
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-07-08 16:10:31 -0400

    finish removing jitsihax

--- js/rollup.config.js
+++ js/rollup.config.js
@@ -4,10 +4,4 @@ export default [{
         file: "bundle.js",
         format: "es"
     }
-}, {
-        input: "src/audio/ExternalJitsiAudioServer.js",
-        output: {
-            file: "jitsi-external-api-hax/jitsihax.js",
-            format: "es"
-        }
-    }];
+}];
