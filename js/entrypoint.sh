baf5048f04506fc94b00454a005cde54b6a234db
mooomooo <mooomooo>
2020-07-08 19:36:16 -0700

    Generalized dockerizing; original constants

---
+++ js/entrypoint.sh
@@ -0,0 +1,17 @@
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+if [[ $JITSI_HOST ]]; then
+  JITSI_HOST="\"$JITSI_HOST\""
+else
+  JITSI_HOST='"jitsi" + window.location.hostname.substring(window.location.hostname.indexOf("."))'
+fi
+DEFAULT_JVB_HOST='meet.jitsi'
+DEFAULT_JVB_MUC='muc.meet.jitsi'
+
+cat > $DIRECTORY/constants.js <<- EOF
+const JITSI_HOST = $JITSI_HOST;
+const JVB_HOST = "${JVB_HOST:-$DEFAULT_JVB_HOST}";
+const JVB_MUC = "${JVB_MUC:-$DEFAULT_JVB_MUC}";
+EOF
+
+exec nginx -g "daemon off;"
