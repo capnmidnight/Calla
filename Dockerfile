66bceb315316b191bf4f0a2371d7359eaa04955a
mooomooo <mooomooo>
2020-07-08 19:36:16 -0700

    Generalized dockerizing; original constants

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM nginx:alpine
-
-RUN rm -rf /usr/share/nginx/html
-
-COPY ./js /usr/share/nginx/html
-
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,3 @@
-const JITSI_HOST = "jitsi" + window.location.hostname.substring(window.location.hostname.indexOf('.'));
-const JVB_HOST = "meet.jitsi";
-const JVB_MUC = "muc.meet.jitsi";
+const JITSI_HOST = "jitsi.calla.chat";
+const JVB_HOST = JITSI_HOST;
+const JVB_MUC = "conference." + JITSI_HOST;
