0f3df4a8c4484b4f2bd14fe3b8528eb6da347929
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-07-10 15:07:09 -0400

    peeking under the covers

---
+++ js/testing/hax.js
@@ -0,0 +1,55 @@
+﻿function hax(parent, name, label, creator) {
+    parent[name] = function (...rest) {
+        const value = creator(rest);
+        const params = rest.map(JSON.stringify).join(", ");
+        console.log("HAAAAX", label, "(", params, ") -> ", value);
+        return value;
+    };
+}
+
+export function haxFunction(parent, name, label) {
+    const oldFunction = parent[name];
+    hax(parent, name, "call " + (label || name),
+        function (rest) {
+            if (this === undefined) {
+                return oldFunction.apply(parent, rest);
+            }
+            else {
+                return oldFunction.apply(this, rest);
+            }
+        });
+}
+
+let haxCounter = 0;
+
+export function haxClass(parent, OldClass, methods) {
+
+    const ctor = `constructor(...rest) {
+        super(...rest);
+        const params = rest.map(JSON.stringify).join(", ");
+        console.log("HAAAAX", "create", "${OldClass.name}", "(", params, ")");
+    }`;
+
+    const meths = [];
+
+    for (let method of methods) {
+        meths.push(`${method}(...rest) {
+        const value = super.${method}(...rest);
+        const params = rest.map(JSON.stringify).join(", ");
+        if(value === undefined) {
+            console.log("HAAAAX", "call", "${OldClass.name}::${method}", "(", params, ")");
+        }
+        else {
+            console.log("HAAAAX", "call", "${OldClass.name}::${method}", "(", params, ") -> ", value);
+        }
+        return value;
+    }`);
+    }
+
+    const script = `return class Hax${Date.now()}${haxCounter++} extends ${OldClass.name} {
+    ${ctor}
+    ${meths.join("\n    ")}
+}`;
+
+    parent[OldClass.name] = (new Function(script))();
+}
---
+++ js/tests/hax/index.js
@@ -0,0 +1,59 @@
+﻿import { haxClass, haxFunction } from "../../testing/hax.js";
+
+haxFunction(navigator.mediaDevices, "getUserMedia");
+
+//haxClass(window, XMLHttpRequest, [
+//    "send",
+//    "open"]);
+
+haxClass(window, RTCPeerConnection, [
+    "addIceCandidate",
+    "addStream",
+    "addTrack",
+    "close",
+    "createAnswer",
+    "createDataChannel",
+    "createOffer",
+    "generateCertificate",
+    "getConfiguration",
+    "getIdentityAssertion",
+    "getReceivers",
+    "getSenders",
+    //"getStats",
+    "getStreamById",
+    "getTransceivers",
+    "removeStream",
+    "removeTrack",
+    "restartIce",
+    "setConfiguration",
+    "setIdentityProvider",
+    "setLocalDescription",
+    "setRemoteDescription"]);
+
+
+
+import { init } from "../../src/init.js";
+import { LibJitsiMeetClient as JitsiClient } from "../../src/jitsi/LibJitsiMeetClient.js";
+import { Button } from "../../src/html/tags.js";
+import { style } from "../../src/html/attrs.js";
+import { onClick } from "../../src/html/evts.js";
+import { openSideTest } from "../../testing/windowing.js";
+import { userNumber } from "../../testing/userNumber.js";
+
+const { login } = init("jitsi.calla.chat", new JitsiClient());
+
+if (userNumber === 1) {
+    document.body.append(
+        Button(
+            "Open Side Test",
+            style({
+                position: "absolute",
+                top: 0,
+                left: "calc(50% - 6em)",
+                right: "calc(50% - 6em)",
+            }),
+            onClick(openSideTest)));
+}
+
+login.roomName = "TestRoom";
+login.userName = `TestUser${userNumber}`;
