62be98d5ddbbfdf38dd7dedff44ce774da92cff6
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-07-15 21:44:08 -0400

    make test windows a touch smaller

--- js/testing/windowing.js
+++ js/testing/windowing.js
@@ -21,8 +21,10 @@ window.addEventListener("unload", () => {
 function openWindow(href, x, y, width, height) {
     windows.push(window.open(href, "_blank", `left=${x},top=${y},width=${width},height=${height}`));
 }
+
 /**
- * Opens a new window with a query string parameter that can be used to differentiate different test instances.*/
+ * Opens a new window with a query string parameter that can be used to differentiate different test instances.
+ **/
 export function openSideTest() {
     const loc = new URL(document.location.href);
     loc.searchParams.set("testUserNumber", userNumber + windows.length + 1);
@@ -30,6 +32,6 @@ export function openSideTest() {
         loc.href,
         window.screenLeft + window.outerWidth,
         0,
-        window.outerWidth,
-        window.outerHeight);
+        window.innerWidth,
+        window.innerHeight);
 }
