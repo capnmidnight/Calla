1ea4b70a0d19aceb324379667a4b2928fbe2e34e
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-07-23 13:34:01 -0400

    fix a warning

--- js/lib/resonance-audio.js
+++ js/lib/resonance-audio.js
@@ -7,7 +7,7 @@
 		var a = factory();
 		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
 	}
-})(this || window, function() {
+})(window, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
