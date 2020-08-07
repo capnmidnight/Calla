34f3d1023560dc859a3370b01f9d98cde030a5c6
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-08-07 00:19:08 -0400

    'yolo'

--- game/version.js
+++ game/version.js
@@ -1,4 +1,4 @@
-const versionString = "Calla v0.5.10";
+const versionString = "Calla v0.5.11";
 
 const c = document.querySelector("#version");
 if (c) {
--- js/src/version.js
+++ js/src/version.js
@@ -1,1 +1,1 @@
-export const versionString = "Calla v0.5.10";
+export const versionString = "Calla v0.5.11";
