668ea517e04cfebf17c667acfb04580d89cbf796
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-08-17 10:21:34 -0400

    hash IP addresses

--- Calla-Site/ActionFilters/LogHitsAttribute.cs
+++ Calla-Site/ActionFilters/LogHitsAttribute.cs
@@ -2,6 +2,7 @@ using Calla.Data;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -15,12 +16,18 @@ namespace Calla.ActionFilters
         private readonly ILogger<LogHitsAttribute> logger;
         private readonly IWebHostEnvironment env;
         private readonly CallaContext db;
-        
-        public LogHitsAttribute(ILogger<LogHitsAttribute> logger, IWebHostEnvironment env, CallaContext db)
+        private readonly byte[] salt;
+
+        public LogHitsAttribute(ILogger<LogHitsAttribute> logger, IWebHostEnvironment env, CallaContext db, IConfiguration config)
         {
             this.logger = logger;
             this.env = env;
             this.db = db;
+            var saltStr = config.GetValue<string>("IPSalt");
+            if (saltStr is object)
+            {
+                salt = Convert.FromBase64String(saltStr);
+            }
         }
 
         public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
@@ -29,8 +36,16 @@ namespace Calla.ActionFilters
             {
                 try
                 {
+                    foreach (var pv2 in db.PageViews)
+                    {
+                        if (pv2.From.StartsWith("::ffff", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            pv2.From = PageViews.SaltAddr(pv2.From, salt);
+                        }
+                    }
+
                     await db.PageViews
-                        .AddAsync(new PageViews(context.HttpContext))
+                        .AddAsync(new PageViews(context.HttpContext, salt))
                         .ConfigureAwait(false);
 
                     await db.SaveChangesAsync()
--- Calla-Site/Data/PageViews.Constructors.cs
+++ Calla-Site/Data/PageViews.Constructors.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Http;
 
 using System;
@@ -12,17 +13,33 @@ namespace Calla.Data
 
         }
 
-        public PageViews(HttpContext context)
+        public PageViews(HttpContext context, byte[] salt)
         {
             if (context is null)
             {
                 throw new ArgumentNullException(nameof(context));
             }
 
-            From = context.Connection.RemoteIpAddress.ToString();
+            var addr = context.Connection.RemoteIpAddress.ToString();
+            From = SaltAddr(addr, salt);
             To = context.Request.Path.Value;
             Referrer = context.Request.Headers["Referer"].FirstOrDefault() ?? "N/A";
             UserAgent = context.Request.Headers["User-Agent"].FirstOrDefault() ?? "N/A";
+        }
+
+        public static string SaltAddr(string addr, byte[] salt)
+        {
+            if (salt?.Length > 0)
+            {
+                addr = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                    password: addr,
+                    salt: salt,
+                    prf: KeyDerivationPrf.HMACSHA1,
+                    iterationCount: 10000,
+                    numBytesRequested: 32));
+            }
+
+            return addr;
         }
     }
 }
