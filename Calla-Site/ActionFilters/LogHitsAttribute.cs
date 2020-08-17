5958e3edd52b04b1dcecb8c363c221fca9c52777
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-08-16 21:42:15 -0400

    don't log page hits in dev

--- Calla-Site/ActionFilters/LogHitsAttribute.cs
+++ Calla-Site/ActionFilters/LogHitsAttribute.cs
@@ -1,6 +1,8 @@
 using Calla.Data;
 
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 using System;
@@ -12,32 +14,37 @@ namespace Calla.ActionFilters
     public class LogHitsAttribute : ActionFilterAttribute
     {
         private readonly ILogger<LogHitsAttribute> logger;
+        private readonly IWebHostEnvironment env;
         private readonly CallaContext db;
-        public LogHitsAttribute(ILogger<LogHitsAttribute> logger, CallaContext db)
+        
+        public LogHitsAttribute(ILogger<LogHitsAttribute> logger, IWebHostEnvironment env, CallaContext db)
         {
             this.logger = logger;
+            this.env = env;
             this.db = db;
         }
 
         public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
         {
-            try
+            if (env.IsProduction())
             {
-                await db.PageViews.AddAsync(new PageViews
+                try
                 {
-                    From = context.HttpContext.Connection.RemoteIpAddress.ToString(),
-                    To = context.HttpContext.Request.Path.Value,
-                    Referrer = context.HttpContext.Request.Headers["Referer"].FirstOrDefault() ?? "N/A"
-                }).ConfigureAwait(false);
+                    await db.PageViews.AddAsync(new PageViews
+                    {
+                        From = context.HttpContext.Connection.RemoteIpAddress.ToString(),
+                        To = context.HttpContext.Request.Path.Value,
+                        Referrer = context.HttpContext.Request.Headers["Referer"].FirstOrDefault() ?? "N/A"
+                    }).ConfigureAwait(false);
 
-                await db.SaveChangesAsync()
-                    .ConfigureAwait(false);
+                    await db.SaveChangesAsync()
+                        .ConfigureAwait(false);
+                }
+                catch (Exception exp)
+                {
+                    logger.LogError(exp, "Error while logging page view");
+                }
             }
-            catch (Exception exp)
-            {
-                logger.LogError(exp, "Error while logging page view");
-            }
-
             await base.OnResultExecutionAsync(context, next)
                 .ConfigureAwait(false);
         }
