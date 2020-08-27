69e2a0f93cb14e88a7e537874092270ade7f0431
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-08-26 22:01:30 -0400

    added dependencies

--- Calla-Site/pull-db.bat
+++ Calla-Site/pull-db.bat
@@ -1,2 +1,2 @@
-dotnet ef dbcontext scaffold Name=ConnectionStrings:Yarrow Npgsql.EntityFrameworkCore.PostgreSQL -o Yarrow.Data --context YarrowContext --force
-dotnet ef dbcontext scaffold Name=ConnectionStrings:Calla Npgsql.EntityFrameworkCore.PostgreSQL -o Calla.Data --context CallaContext --force
+dotnet ef dbcontext scaffold Name=ConnectionStrings:Yarrow Npgsql.EntityFrameworkCore.PostgreSQL -o Data/Yarrow --context YarrowContext --force
+dotnet ef dbcontext scaffold Name=ConnectionStrings:Calla Npgsql.EntityFrameworkCore.PostgreSQL -o Data/Calla --context CallaContext --force
