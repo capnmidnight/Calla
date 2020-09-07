f26e1c48660fbfe38b778dd1bcc6711a2c2d8192
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-09-07 10:47:31 -0400

    option to set debug object's color

--- Calla-Site/scripts/vr/DebugObject.js
+++ Calla-Site/scripts/vr/DebugObject.js
@@ -2,8 +2,11 @@ import { BoxBufferGeometry } from "three/src/geometries/BoxGeometry";
 import { MeshBasicMaterial } from "three/src/materials/MeshBasicMaterial";
 import { Mesh } from "three/src/objects/Mesh";
 
-const cube = new BoxBufferGeometry(0.1, 0.1, 0.1, 1, 1, 1),
-    mube = new MeshBasicMaterial({ color: 0xff0000 });
-export function DebugObject() {
-    return new Mesh(cube, mube);
+const colors = new Map();
+const cube = new BoxBufferGeometry(0.1, 0.1, 0.1, 1, 1, 1);
+export function DebugObject(color = 0xff0000) {
+    if (!colors.has(color)) {
+        colors.set(color, new MeshBasicMaterial({ color }));
+    }
+    return new Mesh(cube, colors.get(color));
 }
