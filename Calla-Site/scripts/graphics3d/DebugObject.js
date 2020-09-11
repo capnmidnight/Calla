d11cd260285705e652e5928ddb4f7b429b2c3ea1
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-09-11 11:33:47 -0400

    a more useful debug object

--- Calla-Site/scripts/graphics3d/DebugObject.js
+++ Calla-Site/scripts/graphics3d/DebugObject.js
@@ -4,9 +4,35 @@ import { Mesh } from "three/src/objects/Mesh";
 
 const colors = new Map();
 const cube = new BoxBufferGeometry(0.1, 0.1, 0.1, 1, 1, 1);
-export function DebugObject(color = 0xff0000) {
+
+/**
+ * @param {string|number|import("three").Color} color
+ */
+export function Cube(color) {
     if (!colors.has(color)) {
         colors.set(color, new MeshBasicMaterial({ color }));
     }
     return new Mesh(cube, colors.get(color));
+}
+
+/**
+ * @param {string|number|import("three").Color} color
+ */
+export function DebugObject(color = 0xff0000) {
+    const center = Cube(color);
+    const x = Cube(0xff0000);
+    const y = Cube(0x00ff00);
+    const z = Cube(0x0000ff);
+
+    x.scale.set(3, 0.1, 0.1);
+    x.position.x = 0.15;
+
+    y.scale.set(0.1, 3, 0.1);
+    y.position.y = 0.15;
+
+    z.scale.set(0.1, 0.1, 3);
+    z.position.z = 0.15;
+
+    center.add(x, y, z);
+    return center;
 }
