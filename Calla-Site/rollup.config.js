27ff4ac39930c3e6f5b9bd798404bf3251ef7468
Sean T. McBeth <sean.mcbeth@gmail.com>
2020-09-15 14:14:17 -0400

    try to kill error logging if there was an error during the log attempt

--- Calla-Site/rollup.config.js
+++ Calla-Site/rollup.config.js
@@ -5,15 +5,24 @@ import { terser } from "rollup-plugin-terser";
 
 const traceKit = fs.readFileSync("node_modules/tracekit/tracekit.js", { encoding: "utf8" });
 const banner = `${traceKit}
-
+(function(){
+var keepTrying = true;
 TraceKit.report.subscribe((err) => {
-    err.userAgent = navigator.userAgent;
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/ErrorLog", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(err));
+    if(keepTrying){
+        try{
+            err.userAgent = navigator.userAgent;
+            const xhr = new XMLHttpRequest();
+            xhr.onerror = function() { keepTrying = false; };
+            xhr.open("POST", "/ErrorLog");
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(err));
+        }
+        catch(exp){
+            keepTrying = false;
+        }
+    }
 });
-
+})();
 try{
 `;
 const footer = `
