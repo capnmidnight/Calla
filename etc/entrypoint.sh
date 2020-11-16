220bf427c5fe8fdfb3de10c8c8d13878ecd2d71d
Saurabh Agarwal <saurabh.agarwal@dremio.com>
2020-11-16 17:09:34 +0530

    Changes to enable security in nginx

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -12,6 +12,12 @@ cat > $DIRECTORY/constants.js <<- EOF
 export const JITSI_HOST = $JITSI_HOST;
 export const JVB_HOST = "${JVB_HOST:-$DEFAULT_JVB_HOST}";
 export const JVB_MUC = "${JVB_MUC:-$DEFAULT_JVB_MUC}";
+
+apk add openssl
+cp /usr/share/nginx/html/nginx-selfsigned.key /etc/ssl/private/nginx-selfsigned.key 
+cp /usr/share/nginx/html/nginx-selfsigned.crt /etc/ssl/certs/nginx-selfsigned.crt 
+mkdir -p /etc/nginx/conf.d 
+cp /usr/share/nginx/html/default.conf /etc/nginx/conf.d/ 
 EOF
 
 exec nginx -g "daemon off;"
