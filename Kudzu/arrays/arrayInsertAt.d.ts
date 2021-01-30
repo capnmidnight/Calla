10f5154d3d1b64bbfc4b0f6a7a98099f0fba780d
Sean T. McBeth <sean.mcbeth@gmail.com>
2021-01-30 17:49:58 -0500

    rebuild

---
+++ Kudzu/arrays/arrayInsertAt.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Inserts an item at the given index into an array.
+ * @param arr
+ * @param item
+ * @param idx
+ */
+export declare function arrayInsertAt<T>(arr: T[], item: T, idx: number): void;
---
+++ Kudzu/arrays/arrayInsertAt.js
@@ -0,0 +1,10 @@
+/**
+ * Inserts an item at the given index into an array.
+ * @param arr
+ * @param item
+ * @param idx
+ */
+export function arrayInsertAt(arr, item, idx) {
+    arr.splice(idx, 0, item);
+}
+//# sourceMappingURL=arrayInsertAt.js.map
