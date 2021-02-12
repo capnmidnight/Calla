d3107f15d0ccc9c4901c1b07f53fc8deb99894ad
Sean T. McBeth <sean.mcbeth@gmail.com>
2021-02-12 13:06:20 -0500

    make all the flags into functions to improve tree-shaking

--- Kudzu/html/flags.js
+++ Kudzu/html/flags.js
@@ -1,24 +1,56 @@
-export const isChrome = "chrome" in globalThis && !navigator.userAgent.match("CriOS");
-export const isFirefox = "InstallTrigger" in globalThis;
-export const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-export const isOpera = /Opera/.test(navigator.userAgent);
-export const isAndroid = /Android/.test(navigator.userAgent);
-export const isIOS = /iPad|iPhone|iPod/.test(navigator.platform)
-    || /Macintosh(.*?) FxiOS(.*?)\//.test(navigator.platform)
-    || navigator.platform === "MacIntel" && navigator.maxTouchPoints > 2;
-export const isBlackberry = /BlackBerry/.test(navigator.userAgent);
-export const isUCBrowser = /(UC Browser |UCWEB)/.test(navigator.userAgent);
-export const isMobile = isAndroid
-    || isIOS
-    || isBlackberry
-    || isUCBrowser;
-export const isDesktop = !isMobile;
-export const isOculus = /oculus/.test(navigator.userAgent);
-export const isOculusGo = isOculus && /pacific/.test(navigator.userAgent);
-export const isOculusQuest = isOculus && /quest/.test(navigator.userAgent);
-export const isMobileVR = /Mobile VR/.test(navigator.userAgent)
-    || isOculusGo
-    || isOculusQuest;
-export const hasWebXR = "xr" in navigator;
-export const hasWebVR = "getVRDisplays" in navigator;
+export function isChrome() {
+    return "chrome" in globalThis && !navigator.userAgent.match("CriOS");
+}
+export function isFirefox() {
+    return "InstallTrigger" in globalThis;
+}
+export function isSafari() {
+    return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+}
+export function isOpera() {
+    return /Opera/.test(navigator.userAgent);
+}
+export function isAndroid() {
+    return /Android/.test(navigator.userAgent);
+}
+export function isIOS() {
+    return /iPad|iPhone|iPod/.test(navigator.platform) || /Macintosh(.*?) FxiOS(.*?)\//.test(navigator.platform) || navigator.platform === "MacIntel" && navigator.maxTouchPoints > 2;
+}
+export function isBlackberry() {
+    return /BlackBerry/.test(navigator.userAgent);
+}
+export function isUCBrowser() {
+    return /(UC Browser |UCWEB)/.test(navigator.userAgent);
+}
+export function isMobile() {
+    return isAndroid()
+        || isIOS()
+        || isBlackberry()
+        || isUCBrowser();
+}
+export function isDesktop() {
+    return !isMobile();
+}
+export function isOculus() {
+    return /oculus/.test(navigator.userAgent);
+}
+export function isOculusGo() {
+    return isOculus()
+        && /pacific/.test(navigator.userAgent);
+}
+export function isOculusQuest() {
+    return isOculus()
+        && /quest/.test(navigator.userAgent);
+}
+export function isMobileVR() {
+    return /Mobile VR/.test(navigator.userAgent)
+        || isOculusGo()
+        || isOculusQuest();
+}
+export function hasWebXR() {
+    return "xr" in navigator;
+}
+export function hasWebVR() {
+    return "getVRDisplays" in navigator;
+}
 //# sourceMappingURL=flags.js.map
