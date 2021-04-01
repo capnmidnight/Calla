511e3b7822e6059d057cccc9f6b28ebecb57f656
Sean T. McBeth <sean.mcbeth@gmail.com>
2021-04-01 01:52:52 -0400

    delete unused files

--- Primrose/flags.js
+++
@@ -1,7 +0,0 @@
-export function isLandscape() {
-    return Math.abs(window.orientation) === 90;
-}
-export function isWorker() {
-    return typeof importScripts === 'function';
-}
-//# sourceMappingURL=flags.js.map
